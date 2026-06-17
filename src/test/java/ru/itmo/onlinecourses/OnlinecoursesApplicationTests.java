2cee09121ee74c6fa9bf7b9d709a34cd6356b380
e4riya <kotkovdima0@gmail.com>
2026-06-17 23:11:28 +0300

    Initial commit

---
+++ src/test/java/ru/itmo/onlinecourses/OnlinecoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.onlinecourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinecoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
